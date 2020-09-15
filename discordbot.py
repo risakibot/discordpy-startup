d00238304dae17eb6e616f5dd5e0ce29eed44fe3
risakibot <71342304+risakibot@users.noreply.github.com>
2020-09-16 02:22:49 +0900

    implement リサキすき

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,10 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+    @bot.command()
+async def リサキすき(ctx):
+    await ctx.send('あたしもすき')
 
 
 bot.run(token)
